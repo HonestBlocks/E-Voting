b8203e603021bdb4e72140e05675c16a6e1b1956
Siddhant Jain <mail.siddhantjain19@gmail.com>
2019-06-03 03:11:01 +0530

    Temprory Cli made with Web3js; able to connect with local blockchain

--- Blockchain/src/index.js
+++ Blockchain/src/index.js
@@ -1,57 +1,11 @@
 const Web3 = require('web3');
-const web3 = new Web3('ws://localhost:7545');
+const web3 = new Web3('http://localhost:7545');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
 const contractaddress = '0x7ce7081433061158b30dd91914629905a9d44a2d'
 const contractABI = [
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "toVoter",
-				"type": "address"
-			}
-		],
-		"name": "giveRightToVote",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_candidate_id",
-				"type": "address"
-			},
-			{
-				"name": "_party",
-				"type": "string"
-			}
-		],
-		"name": "registerCandidates",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_length",
-				"type": "uint256"
-			}
-		],
-		"name": "set_candidates_arr_len",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
 	{
 		"constant": false,
 		"inputs": [
@@ -65,12 +19,6 @@ const contractABI = [
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
 	},
 	{
 		"constant": true,
@@ -98,74 +46,184 @@ const contractABI = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "toVoterr",
+				"type": "string"
+			}
+		],
+		"name": "giveRightToVote",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_length",
+				"type": "uint256"
+			}
+		],
+		"name": "set_candidates_arr_len",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_candidate_id",
+				"type": "address"
+			},
+			{
+				"name": "_party",
+				"type": "string"
+			}
+		],
+		"name": "registerCandidates",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
 	}
 ]
 const mycontract = new web3.eth.Contract(contractABI, contractaddress);
 
-var useraddress
-var promise1 = new Promise(function (resolve, reject) {
-    readline.question(`Enter your address: `, (address) => {
-    useraddress = address;
-    resolve(useraddress);
-    readline.close();
+// console.log(mycontract.jsonInterface);
+
+choices = { 
+            1: 'GiveVoting Rights',
+            2: 'Show Results',
+            3: 'registerCandidates:',
+            4: 'set_candidates_arr_len',
+            5: 'vote'
+
+}
+const askaddress = () => {
+    return new Promise(function (resolve, reject) {
+        readline.question(`\nEnter your address: `, (address) => {
+        var useraddress = address;
+        resolve(useraddress);
   });
 })
-promise1.then(() => {
-    // console.log(useraddress);    
-    // giveVotingRights();
-    // console.log('11!1')
-    var voteraddress;
-    var promise2 = new Promise(function (resolve, reject) {
-        console.log('sese');
-        readline.question(`Enter the address of the voter to give voting Rights: `, (address) =>{
-            readline.close();
-            console.log('asd')
-            voteraddress = address;
-            console.log(voteraddress);
+}
+
+const askchoices = () => {
+    return new Promise(function (resolve, reject) {
+        readline.question(`\nEnter your choice: `, (choice) => {
+        var function_id = choice;
+        resolve(function_id);
+  });
+})
+}
+
+const askvoteraddress = () => {
+    return new Promise(function (resolve, reject) {
+        readline.question(`Enter voter's address: `, (address) => {
+            var voteraddress = address;
             resolve(voteraddress);
-            console.log("!!!!!!")
-            
-        });
-})
-    promise2.then(() => {
-    console.log('herer');
-    mycontract.methods.giveRightToVote()
-    .call({from : useraddress})
-    .then((result) => {
-        console.log('!!!');
-        console.log(result)
-    }).catch((err) => {console.log(err)})
-})
-})
+        })
+    })
+}
 
-// var giveVotingRights = () => {
-//     var voteraddress;
-//     var promise2 = new Promise(function (resolve, reject) {
-//         console.log('sese');
-//         readline.question(`Enter the address of the voter to give voting Rights: `, (address) =>{
-//             console.log('asd')
-//             voteraddress = address;
-//             console.log(voteraddress);
-//             resolve(voteraddress);
-//             console.log("!!!!!!")
-//             readline.close();
-//         });
-// })
-//     promise2.then(() => {
-//     console.log('herer');
-//     mycontract.methods.giveRightToVote()
-//     .call({from : useraddress})
-//     .then((result) => {
-//         console.log('!!!');
-//         console.log(result)
-//     }).catch((err) => {console.log(err)})
-// })
-// }
+const askindex = () => {
+    return new Promise(function(resolve, reject) {
+        readline.question(`Enter index to vote`, (index) => {
+            var candidate_index = parseInt(index,10);
+            resolve(candidate_index);
+         })
+    })
+}
 
+const askcandidateinfo =  () => {
+    return new Promise(function (resolve, reject) {
+        readline.question(`Enter Candidates Address`, (candidate_addr) => {
+            readline.question(`Enter Party name`, (candidate_party) => {
+                var party = candidate_party;
+                var address = candidate_addr;
+            resolve([address, party]); 
+            })
+        })
+    })
+}
 
-// mycontract.methods.candidates_arr(0).call({from : useraddress})
-//         .then((result => {
-//             console.log(result);
-//         }))
-//         .catch((err) => {console.log(err)});
-// promise1
+const main = async () => {
+    let userAddress;
+    let choice;
+    await askaddress().then((address) => {
+        userAddress = address;
+        console.log(userAddress);
+    });
+    await askchoices().then((option) => {
+        choice = option;
+        console.log(typeof(option));
+    });
+    
+    if(choice === '1'){
+        await askvoteraddress().then((voteraddress) =>{
+        mycontract.methods.giveRightToVote(voteraddress)
+            .send({from: userAddress})
+            .on('transactionHash', (transactionHash) => {
+                console.log(transactionHash);
+            })
+        })
+        readline.close();
+    }
+    
+    else if(choice == '2'){
+        await askindex().then((index => {
+        mycontract.methods.candidates_arr(0).call({from : userAddress})
+        .then((result => {
+            console.log(result);
+        }))
+        .catch((err) => {console.log(err)});
+        }))
+    }
+
+    // gas over
+    else if(choice == '3'){
+        await askcandidateinfo().then((arr) => {
+            mycontract.methods.registerCandidates(arr[0],arr[1])
+            .send({from: userAddress})
+            .on('transactionHash', (transactionHash) => {
+                console.log(transactionHash);
+            })
+        })
+    }
+
+    else if(choice == '4'){
+
+    }
+
+    else if(choice == '5'){
+        await askindex().then((index) => {
+            mycontract.methods.vote(index)
+            .send({from: userAddress})
+            .on('transactionHash', (transactionHash) => {
+                console.log(transactionHash);
+            })
+        })
+        readline.close();
+    }
+
+    else{
+        console.log('Select a valid input');
+    }
+
+    readline.close();
+}
+
+main();
