54826c77095e8e619de323245fad2f1637b3eec9
Siddhant Jain <mail.siddhantjain19@gmail.com>
2019-06-09 10:48:21 +0530

    Prior to deply script

--- Blockchain/migrations/2_deploy_contracts.js
+++ Blockchain/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var Election =  artifacts.require("Election");
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
+const Election = artifacts.require("Election.sol");
 
-module.exports = function(deployer){
-deployer.deploy(Election);
-}
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+  deployer.deploy(Election);
+};
